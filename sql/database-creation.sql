3885983bae799e50a7bae955dfffc4e800517f4d
pgoodjohn <bongiovanni.pietro.vc@gmail.com>
2016-08-08 23:53:45 +0200

    Added structural vp candidacy, finalized other candidates

--- sql/database-creation.sql
+++ sql/database-creation.sql
@@ -26,6 +26,13 @@ CREATE TABLE `InternalVicePresidency` (
 		`Votes` INT NOT NULL , 
 		PRIMARY KEY (`UserId`)) ENGINE = InnoDB;
 
+CREATE TABLE `StructVicePresidency` ( 
+		`UserId` INT NOT NULL AUTO_INCREMENT , 
+		`Name` VARCHAR(64) NOT NULL , 
+		`Votes` INT NOT NULL , 
+		PRIMARY KEY (`UserId`)) ENGINE = InnoDB;
+
+
 CREATE TABLE `EditorInChief` ( 
 		`UserId` INT NOT NULL AUTO_INCREMENT , 
 		`Name` VARCHAR(64) NOT NULL , 
@@ -44,11 +51,13 @@ CREATE TABLE `Treasurer` (
 		`Votes` INT NOT NULL , 
 		PRIMARY KEY (`UserId`)) ENGINE = InnoDB;
 
-INSERT INTO `Presidency` (`UserId`, `Name`, `Votes`) VALUES ('1', 'Mu-hamid Pathan', '0'), ('2', 'Kuba Sta', '0'), ('3', 'Robert Zielonka', '0');
+INSERT INTO `Presidency` (`UserId`, `Name`, `Votes`) VALUES ('1', 'Mu-hamid Pathan', '0'), ('2', 'Ido Nathaniel', '0'), ('3', 'Robert Zielonka', '0');
 
-INSERT INTO `ExternalVicePresidency` (`UserId`, `Name`, `Votes`) VALUES ('1', 'Cecilia Passaniti', '0'), ('2', 'Margaux Vidal', '0');
+INSERT INTO `ExternalVicePresidency` (`UserId`, `Name`, `Votes`) VALUES ('1', 'Cecilia Passaniti', '0'), ('2', 'Margaux Vidal', '0'),('3', 'Dearbhla Hone', '0');
 
-INSERT INTO `InternalVicePresidency` (`UserId`, `Name`, `Votes`) VALUES ('1', 'Eva Amalija Oreskovic', '0'), ('2', 'Kritjan Kalve', '0'), ('3', 'Patrik Bole', '0');
+INSERT INTO `InternalVicePresidency` (`UserId`, `Name`, `Votes`) VALUES ('1', 'Eva Amalija Oreskovic', '0'), ('2', 'Kristjan Kalve', '0'), ('3', 'Patrik Bole', '0'), ('4', 'Alexander Kuye', '0');
+
+INSERT INTO `StructVicePresidency` (`UserId`, `Name`, `Votes`) VALUES ('1', 'Kuba Sta', '0');
 
 INSERT INTO `EditorInChief` (`UserId`, `Name`, `Votes`) VALUES ('1', 'Tjeerd Ritmeester', '0'), ('2', 'Lauren Davies', '0');
 
