5af971dd39e3bd5e2c5bbfb026caef964bacdcdb
pgoodjohn <bongiovanni.pietro.vc@gmail.com>
2016-08-11 17:12:03 +0200

    Fixed email body, changed database update method

--- php/register.php
+++ php/register.php
@@ -68,25 +68,33 @@
 
 		$token = Generate16CharId();
 
-		echo '<b>Token:</b> ' . $token . '<br>';
+		echo '<b>Password:</b> ' . $token . '<br>';
 
-		$mailMessage = '<html><body><p>In order to access the voting page you will need the following credentials:<br>';
+		$mailMessage = '<html><body><h1>Attention!</h1><h3>These are the official voting credentials.</h3><p>In order to access the voting page you will need the following credentials:<br>';
 		$mailMessage .= '<b>Name: </b>' . ucwords($name) . '<br>';
 		$mailMessage .= '<b>Email: </b>' . $email . '<br>';
-		$mailMessage .= '<b>Token: </b>' . $token . '<br>';
-		$mailMessage .= 'In order to vote, click <a href="bit.ly/graduatesOfDemocracy2016">here</a> or copy the following link in your address bar: <br> bit.ly/graduatesOfDemocracy2016</p></body></html>';
+		$mailMessage .= '<b>Password: </b>' . $token . '<br>';
+		$mailMessage .= 'In order to vote, click <a href="bit.ly/graduatesOfDemocracy2016">here</a> or copy the following link in your address bar: <br> bit.ly/graduatesOfDemocracy2016</p><p>Voting will be open from 10 August 2016, 08:00 CET to 14 August 2016, 23:59 CET.</p>
+			<p>Please, do not open the link beforehand.</p></body></html>';
 
 		$mailObject = 'Graduates Of Democracy Online Voting Platform Login Details';
 
-		$mailHeaders = "From: noreply@graduatesofdemocracy.com\r\n";
+		$mailHeaders = "From: registration@graduatesofdemocracy.com\r\n";
 		$mailHeaders .= "MIME-Version: 1.0\r\n";
 		$mailHeaders .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-		mail($email, $mailObject, $mailMessage, $mailHeaders);
+		if(mail($email, $mailObject, $mailMessage, $mailHeaders)){
+			print_r($mailMessage);
+		} else{
+			echo 'failure!';
+		}
 		
 		$encryptedToken = hash('sha512', $token);
 
-		createUserRow($name, $email, $encryptedToken);
+		$query = "UPDATE  `User` SET  `Code` =  '$encryptedToken' WHERE  `User`.`Email` ='$email'";
+
+		mysqli_query($con, $query);
+
 		
 		header("Location:../register.php");
 		exit;
