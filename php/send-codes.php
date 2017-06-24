9562cc78c383435168bb632d1528fea3cc2fe0c3
pgoodjohn <pietro@fixico.nl>
2017-06-24 18:29:07 +0200

    PHP Mailer init

--- php/send-codes.php
+++ php/send-codes.php
@@ -1,9 +1,20 @@
 <?php
 
 include 'scripts.php';
+include 'PHPMailer/PHPMailerAutoload.php';
 
 $query = "SELECT * FROM User";
 $result = mysqli_query($con, $query);
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->Host = '	smtp.mailtrap.io';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = '6a0b9f3e7808fa';                 // SMTP username
+$mail->Password = '03c2a293843478';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 25;                                    // TCP port to connect to
+$mail->Subject = 'Graduates Of Democracy Online Voting Platform Login Details';
+$mail->setFrom('noreply@graduates.com', 'Graduates Mailer');
 while($row = mysqli_fetch_array($result)){
 		print_r($row);
 
@@ -21,6 +32,8 @@ while($row = mysqli_fetch_array($result)){
 
 		echo '<b>Password:</b> ' . $token . '<br>';
 
+		$mail->addAddress($email, ucwords($name));     // Add a recipient
+
 		$mailMessage = '<html><body><h1>Attention!</h1><h3>These are the official voting credentials.</h3><p>In order to access the voting page you will need the following credentials:<br>';
 		$mailMessage .= '<b>Name: </b>' . ucwords($name) . '<br>';
 		$mailMessage .= '<b>Email: </b>' . $email . '<br>';
@@ -28,16 +41,13 @@ while($row = mysqli_fetch_array($result)){
 		$mailMessage .= 'In order to vote, click <a href="bit.ly/graduatesOfDemocracy2016">here</a> or copy the following link in your address bar: <br> bit.ly/graduatesOfDemocracy2016</p><p>Voting will be open from 10 August 2016, 08:00 CET to 14 August 2016, 23:59 CET.</p>
 			<p>Please, do not open the link beforehand.</p></body></html>';
 
-		$mailObject = 'Graduates Of Democracy Online Voting Platform Login Details';
+		$mail->Body = $mailMessage;
 
-		$mailHeaders = "From: registration@graduatesofdemocracy.com\r\n";
-		$mailHeaders .= "MIME-Version: 1.0\r\n";
-		$mailHeaders .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-		if(mail($email, $mailObject, $mailMessage, $mailHeaders)){
-			print_r($mailMessage);
-		} else{
-			echo 'failure!';
+		if(!$mail->send()) {
+		    echo 'Message could not be sent.';
+		    echo 'Mailer Error: ' . $mail->ErrorInfo;
+		} else {
+		    echo 'Message has been sent';
 		}
 		
 		$encryptedToken = hash('sha512', $token);
